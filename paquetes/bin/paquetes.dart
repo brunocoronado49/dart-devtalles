8264b8cb8be32df5d762f2089ab6a3637b6c5b82
Bruno Coronado <francorangelcoronado@gmail.com>
2023-05-06 16:54:32 -0600

    sacando la data

--- paquetes/bin/paquetes.dart
+++ paquetes/bin/paquetes.dart
@@ -1,5 +1,15 @@
-import 'package:paquetes/paquetes.dart' as paquetes;
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
 
 void main(List<String> arguments) {
-  print('Hello world: ${paquetes.calculate()}!');
+  final url = Uri.parse('https://reqres.in/api/users?page=2');
+
+  http.get(url).then((res) {
+    final body = jsonDecode(res.body);
+    print('Body: $body');
+    print('Page: ${body["page"]}');
+    print('Per_page: ${body["per_page"]}');
+    print('ID tercer elemento: ${body["data"][2]["id"]}');
+  });
 }

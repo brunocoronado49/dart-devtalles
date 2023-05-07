e56e2381581223bd5cff51fda878e0191b83a1e6
Bruno Coronado <francorangelcoronado@gmail.com>
2023-05-06 20:40:39 -0600

    Terminando el curso de dart

--- paquetes/bin/paquetes.dart
+++ paquetes/bin/paquetes.dart
@@ -1,15 +1,5 @@
-import 'dart:convert';
-
-import 'package:http/http.dart' as http;
+import 'package:paquetes/paquetes.dart' as paquete;
 
 void main(List<String> arguments) {
-  final url = Uri.parse('https://reqres.in/api/users?page=2');
-
-  http.get(url).then((res) {
-    final body = jsonDecode(res.body);
-    print('Body: $body');
-    print('Page: ${body["page"]}');
-    print('Per_page: ${body["per_page"]}');
-    print('ID tercer elemento: ${body["data"][2]["id"]}');
-  });
+  paquete.getreqResRespuestaService();
 }
--- paquetes/lib/paquetes.dart
+++ paquetes/lib/paquetes.dart
@@ -1,1 +1,14 @@
+import 'package:http/http.dart' as http;
+import 'package:paquetes/clases/reqres.dart';
 
+void getreqResRespuestaService() {
+  final url = Uri.parse('https://reqres.in/api/users?page=2');
+
+  http.get(url).then((res) {
+    final resreqresponse = reqResRespuestaFromJson(res.body);
+
+    print('Page: ${resreqresponse.page}');
+    print('Per_page: ${resreqresponse.perPage}');
+    print('Per_page: ${resreqresponse.data[2].id}');
+  });
+}
